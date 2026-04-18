cabf16ad37dde3507227956335e04e624ef6bd54
jack9761 <86904468+jack9761@users.noreply.github.com>
2026-04-18 12:50:52 -0700

    Dragging markers works

--- AngularFrontend/src/app/map-component/map-component.ts
+++ AngularFrontend/src/app/map-component/map-component.ts
@@ -1,7 +1,7 @@
 import { Component, effect, inject, OnInit } from '@angular/core';
 import { LeafletDirective, LeafletLayersControlDirective, LeafletBaseLayersDirective } from '@bluehalo/ngx-leaflet';
 import * as L from "leaflet";
-import { Control, map } from 'leaflet';
+import { Control, icon, map } from 'leaflet';
 import { StationService } from '../station-service';
 import { StaticDataEditingService } from '../static-data-editing-service';
 @Component({
@@ -39,6 +39,7 @@ export class MapComponent implements OnInit {
   onMapReady(map: L.Map) {
     this.map = map;
     this.station_marker_group.addTo(this.map);
+    this.updateMarkers();
   }
   ngOnInit(): void {
     
@@ -46,28 +47,40 @@ export class MapComponent implements OnInit {
   constructor(){
     effect(() => {
       const stations = this._station_service.stations();
-      if(stations && this.map) {
-        this.station_marker_group.clearLayers();
-        stations.forEach(station => {
-          const marker = L.marker([station.StaticStationData.latitude, station.StaticStationData.longitude],{draggable: true}).addTo(this.station_marker_group)
-          this.station_marker_map.set(station.StaticStationData.id, marker);
-          marker.dragging?.disable();
-        });
+      if(stations) {
+        this.updateMarkers();
       }
     });
     effect(() => {
       const editing_station = this._static_data_editing_service.editing_station();
-      if(editing_station && this.map) {
-        const marker = this.station_marker_map.get(editing_station.StaticStationData.id);
+      if(this._static_data_editing_service.editing_station() && this.map) {
+        const marker = this.station_marker_map.get(editing_station!.StaticStationData.id);
         if(marker && marker.dragging) {
           marker.on("drag", () => {
             const latlng = marker.getLatLng();
-            editing_station.StaticStationData.latitude = latlng.lat;
-            editing_station.StaticStationData.longitude = latlng.lng;
+            this._static_data_editing_service.editing_station.set({
+              ...this._static_data_editing_service.editing_station()!,
+              StaticStationData: {
+                ...this._static_data_editing_service.editing_station()!.StaticStationData,
+                latitude: latlng.lat,
+                longitude: latlng.lng,
+              }
+            });
           });
           marker.dragging.enable();
         }
       }
     });
+  }
+  updateMarkers(){
+      const stations = this._station_service.stations();
+      if(stations && this.map) {
+        this.station_marker_group.clearLayers();
+        stations.forEach(station => {
+          const marker = L.marker([station.StaticStationData.latitude, station.StaticStationData.longitude],{draggable: true, icon: icon({...L.Icon.Default.prototype.options,iconUrl: 'assets/marker-icon.png',iconRetinaUrl: 'assets/marker-icon-2x.png',shadowUrl: 'assets/marker-shadow.png'})}).addTo(this.station_marker_group)
+          this.station_marker_map.set(station.StaticStationData.id, marker);
+          marker.dragging?.disable();
+        });
+      }
   }
 }
